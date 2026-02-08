7fbf1c7d171d23b645b9a30b7a02c9512335b06d
bigbox <unic0rn9k@protonmail.com>
2026-02-08 15:46:52 +0100

    ruff format model

--- src/tokentango/model.py
+++ src/tokentango/model.py
@@ -6,24 +6,34 @@ import math
 
 EMBEDDING_DIM = 32
 
+
 class BertClassifier(nn.Module):
     def __init__(self, max_seq_len, vocab_size, device):
         super(BertClassifier, self).__init__()
-        #self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased', output_attentions=False, output_hidden_states=False)
-        #self.out = nn.Linear(self.bert.config.dim, 1)
-        
+        # self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased', output_attentions=False, output_hidden_states=False)
+        # self.out = nn.Linear(self.bert.config.dim, 1)
+
         self.vocab_size = vocab_size
         self.device = device
 
         self.encoder_layer = nn.TransformerEncoderLayer(d_model=EMBEDDING_DIM, nhead=4)
-        self.use_nested_tensor=True
-        self.encoder_layer.self_attn.batch_first=True
-        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=6, norm = nn.LayerNorm(EMBEDDING_DIM))
+        self.use_nested_tensor = True
+        self.encoder_layer.self_attn.batch_first = True
+        self.transformer_encoder = nn.TransformerEncoder(
+            self.encoder_layer, num_layers=6, norm=nn.LayerNorm(EMBEDDING_DIM)
+        )
 
-        self.embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=EMBEDDING_DIM)#, padding_idx=0)
-        self.positional_encoding = torch.zeros([max_seq_len, EMBEDDING_DIM])#, requires_grad=True)
+        self.embeddings = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=EMBEDDING_DIM
+        )  # , padding_idx=0)
+        self.positional_encoding = torch.zeros(
+            [max_seq_len, EMBEDDING_DIM]
+        )  # , requires_grad=True)
         position = torch.arange(0, max_seq_len).unsqueeze(1).float()
-        div_term = torch.exp(torch.arange(0, EMBEDDING_DIM, 2).float() * -(math.log(10000.0) / EMBEDDING_DIM))
+        div_term = torch.exp(
+            torch.arange(0, EMBEDDING_DIM, 2).float()
+            * -(math.log(10000.0) / EMBEDDING_DIM)
+        )
         self.positional_encoding[:, 0::2] = torch.sin(position * div_term)
         self.positional_encoding[:, 1::2] = torch.cos(position * div_term)
 
@@ -34,9 +44,13 @@ class BertClassifier(nn.Module):
 
     def embed(self, seq):
         x = self.embeddings(seq)
-        expanded_positional_encoding = self.positional_encoding.unsqueeze(0)  # Shape: (1, sequence_length, embedding_size)
-        expanded_positional_encoding = expanded_positional_encoding.expand(x.shape[0], -1, -1).to(self.device)  # Shape: (batch_size, sequence_length, embedding_size)
-        x += expanded_positional_encoding[:x.size(0), :x.size(1), :x.size(2)]
+        expanded_positional_encoding = self.positional_encoding.unsqueeze(
+            0
+        )  # Shape: (1, sequence_length, embedding_size)
+        expanded_positional_encoding = expanded_positional_encoding.expand(
+            x.shape[0], -1, -1
+        ).to(self.device)  # Shape: (batch_size, sequence_length, embedding_size)
+        x += expanded_positional_encoding[: x.size(0), : x.size(1), : x.size(2)]
         return x
 
     def hidden(self, seq, **kwargs):
@@ -48,12 +62,12 @@ class BertClassifier(nn.Module):
 
     def classify_loss(self, hidden, mb_y):
         logits = self.classify(hidden)
-        loss_cls = torch.nn.functional.huber_loss(
-            logits.view(-1), mb_y
-        )
+        loss_cls = torch.nn.functional.huber_loss(logits.view(-1), mb_y)
         return loss_cls
 
     def mlm_loss(self, hidden, mb_x):
-        outputs = self.distribution(hidden)[:,1:,:]
-        loss_mlm = torch.nn.functional.cross_entropy(outputs.reshape(-1, self.vocab_size), mb_x[:,1:].reshape(-1))
+        outputs = self.distribution(hidden)[:, 1:, :]
+        loss_mlm = torch.nn.functional.cross_entropy(
+            outputs.reshape(-1, self.vocab_size), mb_x[:, 1:].reshape(-1)
+        )
         return loss_mlm
