385fbe91d6535a905a48195e27757954d8cb302b
bigbox <unic0rn9k@protonmail.com>
2026-02-22 14:51:41 +0100

    visualize MLM comp results

--- notebooks/visualize_mlm.py
+++ notebooks/visualize_mlm.py
@@ -7,6 +7,109 @@ app = marimo.App(width="full")
 @app.cell
 def _():
     import tokentango as tt
+    import plotly.graph_objects as go
+    import datetime as dt
+    import numpy as np
+    from copy import copy
+    return copy, dt, go, np, tt
+
+
+@app.cell
+def _():
+    runs_of_interest = {'silly-llama-03248e', 'jolly-quokka-b92ef4'}
+    return (runs_of_interest,)
+
+
+@app.cell
+def _(tt):
+    checkpoints = tt.train.list_checkpoints(meta_only=True)
+    return (checkpoints,)
+
+
+@app.cell
+def _(checkpoints):
+    set([c.config.run_name for c in checkpoints])
+    return
+
+
+@app.cell
+def _(checkpoints, runs_of_interest):
+    mlm_run = [c for c in reversed(checkpoints) if c.config.run_name in runs_of_interest and c.config.use_mlm]
+    cls_run = [c for c in reversed(checkpoints) if c.config.run_name in runs_of_interest and not c.config.use_mlm]
+    return cls_run, mlm_run
+
+
+@app.cell
+def _(cls_run, mlm_run):
+    len(cls_run) / len(mlm_run)
+    return
+
+
+@app.cell
+def _(cls_run, dt, mlm_run, np):
+    mlm_ts = np.array([dt.datetime.strptime(c.timestamp, '%Y-%m-%d_%H-%M-%S') for c in mlm_run])
+    cls_ts = np.array([dt.datetime.strptime(c.timestamp, '%Y-%m-%d_%H-%M-%S') for c in cls_run])
+
+    mlm_ts -= mlm_ts[0]
+    cls_ts -= cls_ts[0]
+
+    mlm_ts = np.array([t.total_seconds() / 60 for t in mlm_ts], dtype=float)
+    cls_ts = np.array([t.total_seconds() / 60 for t in cls_ts], dtype=float)
+    return cls_ts, mlm_ts
+
+
+@app.cell
+def _(cls_run, cls_ts, go, mlm_run, mlm_ts):
+    go.Figure(data=[
+        go.Scatter(x=mlm_ts, y=[c.accuracy for c in mlm_run], name="MLM + CLS"),
+        go.Scatter(x=cls_ts, y=[c.accuracy for c in cls_run], name="CLS only"),
+    ]).update_layout(
+        xaxis_title="Training duration (minutes)",
+        yaxis_title="Accuracy on CLS test-set (%)",
+    )
+    return
+
+
+@app.cell
+def _(cls_ts, mlm_ts, np):
+    def interp_100(arr):
+        n = len(arr)
+        return np.interp(
+            np.linspace(0, n - 1, (n - 1) * 100 + 1),
+            np.arange(n),
+            arr
+        )
+
+    mlm_ts2 = interp_100(mlm_ts)
+    cls_ts2 = interp_100(cls_ts)
+    return cls_ts2, mlm_ts2
+
+
+@app.cell
+def _(cls_run, copy, mlm_run, np):
+    mlm_loss = [copy(c.cls_losses) for c in mlm_run]
+    cls_loss = [copy(c.cls_losses) for c in cls_run]
+
+    mlm_loss[0].pop(0)
+    cls_loss[0].pop(0)
+
+    mlm_loss = np.array(mlm_loss)
+    cls_loss = np.array(cls_loss)
+    return cls_loss, mlm_loss
+
+
+@app.cell
+def _(cls_loss, cls_ts, cls_ts2, go, mlm_loss, mlm_ts, mlm_ts2):
+    go.Figure(data=[
+        go.Scatter(x=mlm_ts2, y=mlm_loss.flatten(), name="MLM + CLS"),
+        go.Scatter(x=cls_ts2, y=cls_loss.flatten(), name="CLS only"),
+
+        go.Scatter(x=mlm_ts, y=mlm_loss.mean(axis=1), name="avg MLM + CLS"),
+        go.Scatter(x=cls_ts, y=cls_loss.mean(axis=1), name="avg CLS only"),
+    ]).update_layout(
+        xaxis_title="Training duration (minutes)",
+        yaxis_title="Accuracy on classification task (%)",
+    )
     return
 
 
